1bfc61e9db3845da25b2d71c115865c039b6f755
ChungYuTsai <jackytsai333@gmail.com>
2021-01-18 22:13:01 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WeatherApp from './WeatherApp';
+import * as serviceWorker from './serviceWorker'
 
 // 這支 CSS 檔的樣式會作用到全域
 import './styles.css';
@@ -11,3 +12,5 @@ function App() {
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
+
+serviceWorker.register();
