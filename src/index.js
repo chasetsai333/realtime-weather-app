37c4e3d3823f5ac2b03e8429ac37fb2a8d37a78b
ChungYuTsai <jackytsai333@gmail.com>
2021-01-28 17:09:44 +0800

    sw

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WeatherApp from './WeatherApp';
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorkerRegistration'
 
 // 這支 CSS 檔的樣式會作用到全域
 import './styles.css';
