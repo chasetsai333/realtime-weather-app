346120ac0ed44a2969b3fc0366e74493eb014ff0
ChungYuTsai <jackytsai333@gmail.com>
2021-01-28 17:34:21 +0800

    sw4

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -34,6 +34,7 @@ export function register(config) {
     const doRegister = () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
+      console.log(config)
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
